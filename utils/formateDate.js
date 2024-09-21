9f37790616028548403c875827f2777da08a6a46
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-09-21 12:43:41 +0600

    createAt time convert locat time

---
+++ utils/formateDate.js
@@ -0,0 +1,12 @@
+
+export const formateDate = (isoDate) =>{
+    const date = new Date(isoDate);
+    return date.toLocaleDateString('en-us',{
+        year: 'numeric',
+        month: 'long',
+        day: 'numeric'
+        
+    })
+
+
+}
