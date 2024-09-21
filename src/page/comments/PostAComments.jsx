5c1a2d6595ac6222759f833bc4fa64410d3b609e
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-09-21 09:30:42 +0600

    post comment

--- src/page/comments/PostAComments.jsx
+++ src/page/comments/PostAComments.jsx
@@ -1,13 +1,23 @@
-
-import {useParams} from 'react-router-dom'
+import { useState } from "react";
 
 const PostAComments = () => {
-    const {id} = useParams() 
-    return (
-        <div>
-            posr a comment s
-        </div>
-    );
+//   const { id } = useParams();
+  const [comments, setComments] = useState("");
+  return (
+    <div>
+      <h1 className="text-lg font-medium mb-8"></h1>
+      <form>
+        <textarea
+          name="text"
+          value={comments}
+          onChange={(e) => setComments(e.target.value)}
+          cols={"30"}
+          rows={"10"}
+          placeholder="Share your opinion about this ..."
+        />
+      </form>
+    </div>
+  );
 };
 
 export default PostAComments;
