c1c1999ca00072bab29402f68312ba99be329edd
Tanvir islam <142686547+TANVIR0222@users.noreply.github.com>
2024-09-21 09:31:57 +0600

    add redux provider

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,15 @@ import App from "./App.jsx";
 import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import { router } from "./router/Router.jsx";
+import { Provider } from 'react-redux'
+import { store } from "./redux/store.js";
 
 createRoot(document.getElementById("root")).render(
+      <Provider store={store}>
   <StrictMode>
     <div className="max-w-screen-xl bg-bgPrimary mx-auto">
-      <RouterProvider router={router} />
+        <RouterProvider router={router} />
     </div>
   </StrictMode>
+    </Provider>
 );
